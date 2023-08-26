00ff3f5bd4404885c2c9619efa0c3baef8ab0384
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-08-26 16:50:45 +0530

    Update logout.js

--- session auth/routes/logout.js
+++ session auth/routes/logout.js
@@ -1,11 +1,7 @@
 const express=require('express');
-const path=require('path');
 const app=express();
 const cookieparser=require('cookie-parser')
-app.set('views',path.join(__dirname , 'views'))
-app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
-app.use(express.json())
 const router=express.Router();
 app.use(cookieparser());
 const mongoose=require('mongoose')
